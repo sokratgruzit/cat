49d268f341c16ce1a521fa3022ed7f5161b4b0fc
sokratgruzit <tavadzed@gmail.com>
2025-05-02 23:12:14 +0400

    first commit

--- src/components/UI/languages/Languages.tsx
+++ src/components/UI/languages/Languages.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useTranslation } from "react-i18next";
 
